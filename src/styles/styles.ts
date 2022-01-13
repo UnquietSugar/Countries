3f3482f2e4a64dfaf7e67476846901fd6b9edf18
Kiril Konstandi <kiril.konstandi@visma.com>
2022-01-13 14:48:07 +0200

    Add. styles.ts

---
+++ src/styles/styles.ts
@@ -0,0 +1,9 @@
+const styles = {
+  tableContainerSx: { display: 'flex', flexDirection: 'column', alignItems: 'center', paddingY: 10 },
+  tableSx: { maxWidth: 800, minWidth: 400 },
+  tablePaginationSx: { marginTop: '-50px' },
+  paginationDiv: { display: 'flex', justifyContent: 'center', marginTop: '-50px' },
+  loaderBoxSx: { display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh' },
+}
+
+export default styles;
